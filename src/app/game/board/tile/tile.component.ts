b9d46e95e5c317411547f54ec3cf163de655d597
meavrik <meavrik@gmail.com>
2018-05-30 13:14:23 +0300

    fix animation

--- src/app/game/board/tile/tile.component.ts
+++ src/app/game/board/tile/tile.component.ts
@@ -39,14 +39,6 @@ import { IState } from '../../../redux/interfaces';
       transition('* => up', animate('150ms ease-out')),
       transition('* => down', animate('150ms ease-out'))
     ]),
-
-    trigger('terrainAnimation', [
-      animate(300, keyframes([
-        style({opacity: 1, offset: 0}),
-        style({opacity: 0, offset: 0.5}),
-        style({opacity: 1, offset: 1.0})
-      ]))
-    ]),
   ]
 })
 
